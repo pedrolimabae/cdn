90a795636cd5a3169072841f8ea5b37a2fe8eba6
unknown <mailsonlyma7@gmail.com>
2018-05-09 19:12:12 -0300

    drgtdrt

--- DB/index.js
+++ DB/index.js
@@ -7,8 +7,9 @@ var $mongo = (function(){
      function getInstanceStorage(callback){
         if(connStorage) return callback(null, Grid(connStorage, mongodb));
 
-        MongoClient.connect('mongodb://AdminStorageGS:ughrUGHRGrgt__rtjdr$H8734tGUYhhyg86@159.203.90.22:27017/storage',{autoReconnect : true}, function(err, client){
+        MongoClient.connect('mongodb://AdminStorageGS:dfgidfuhYFGYGYU865gGYF76tgg76T76GY@db.granasocial.com.br:27017/storage',{autoReconnect : true}, function(err, client){
         //MongoClient.connect('mongodb://127.0.0.1:27017',{autoReconnect : true}, function(err, client){
+            log(err)
 
             if(err) return callback(ErrorDB());
             connStorage = client.db('storage');
--- boot/index.js
+++ boot/index.js
@@ -5,6 +5,27 @@ website = {
 }
 log = console.log;
 
+ErrorDB = function(){
+     return Exit('Ops, algo deu errado!','Estamos em manutenção, caso o erro demorar mais que 2 horas contate o suporte!', 400);
+}
+ 
+Exit = function(title, message, status){
+    if(!title) title = 'Erro encontrado';
+    if(!message) message = 'Não foi possível completar a sua solicitação';
+    var exit = new Error(title);
+    exit.info = message;
+    exit.exitApp=1;
+    exit.status = (status ? status : 200);
+    return exit;
+};
+Json = function($title, $info, $array){
+    var exit = Exit($title, $info);
+    exit.data = $array;
+    return exit;
+};
+ErrorDF = function(status){
+    return Exit(null,null,status);
+}
 Model = require(ROOT+'/models/index');
 
 Storage = require(ROOT+'/models/storage/index');
