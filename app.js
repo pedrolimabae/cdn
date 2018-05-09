4d78936bb5e60c9a01a94e8a47984458a773ea51
unknown <mailsonlyma7@gmail.com>
2018-05-09 18:39:51 -0300

    dfgd

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
+
+
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
