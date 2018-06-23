6cfec81b65e7b96d2380080ef6101cc843537ae7
Mailson <mailsonbae@gmail.com>
2018-06-22 21:43:46 -0300

    rtgdrtgd

--- DB/index.js
+++ DB/index.js
@@ -7,12 +7,12 @@ var $mongo = (function(){
      function getInstanceStorage(callback){
         if(connStorage) return callback(null, Grid(connStorage, mongodb));
 
-        //MongoClient.connect('mongodb://AdminStorageGS:dfgidfuhYFGYGYU865gGYF76tgg76T76GY@db.granasocial.com.br:27017/storage',{autoReconnect : true}, function(err, client){
-        MongoClient.connect('mongodb://127.0.0.1:27017',{autoReconnect : true}, function(err, client){
+        MongoClient.connect('mongodb://root:NuIdomIarrawte3@ds115931.mlab.com:15931/granasocial',{autoReconnect : true}, function(err, client){
+        //MongoClient.connect('mongodb://127.0.0.1:27017',{autoReconnect : true}, function(err, client){
             log(err)
 
             if(err) return callback(ErrorDB());
-            connStorage = client.db('storage');
+            connStorage = client.db('granasocial');
             callback(null, Grid(connStorage, mongodb));
         });
     }
